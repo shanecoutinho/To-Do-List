547fe29d4f515d04b6111406e1af4b408ee31ecb
Shane Coutinho <coutinhoshane7@gmail.com>
2024-05-21 22:42:36 +0200

    Called the function on where changes need saving

--- main.js
+++ main.js
@@ -12,15 +12,15 @@ function addTask(){
         li.appendChild(span);
     }
     inputBox.value = "";   
-    
+    saveTask();
 };
 listContainer.addEventListener("click",function(e){
     if (e.target.tagName === "LI"){
         e.target.classList.toggle("checked")
-    
+        saveTask();
     }else if (e.target.tagName === "SPAN"){
         e.target.parentElement.remove();
-    
+        saveTask();
     }
 
 },false)
